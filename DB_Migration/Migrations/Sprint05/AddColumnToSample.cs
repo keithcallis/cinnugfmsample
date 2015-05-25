b1bb97044d36d9e2ab281c21feffc598d6b63ead
KCallis <keith.callis@sds-consulting.com>
2015-05-24 22:38:54 -0400

    [none] Add columns to tables

---
+++ DB_Migration/Migrations/Sprint05/AddColumnToSample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentMigrator;
+
+namespace DB_Migration.Migrations.Sprint05
+{
+    [Migration(201505242158, TransactionBehavior.Default)]
+    public class AddColumnToSample : Migration
+    {
+        public override void Up()
+        {
+            if (!Schema.Table("Sample").Column("IsActive").Exists())
+            {
+                Create.Column("IsActive").OnTable("Sample").InSchema("dbo")
+                    .AsBoolean().NotNullable().SetExistingRowsTo(1);
+
+                Update.Table("Sample").InSchema("dbo")
+                    .Set(new {IsActive = 0, Column2 = DateTime.Now.Date})
+                    .Where(new {Column1 = "Simple Order Fee"});
+
+                Insert.IntoTable("Sample").InSchema("dbo")
+                    .Row(new { Column1 = "Entry Order Fee", Column2 = DateTime.Now.AddDays(-7), Column3 = 0.99m, IsActive = 1 });
+                Insert.IntoTable("Sample").InSchema("dbo")
+                    .Row(new { Column1 = "Repeat Order Fee", Column2 = DateTime.Now.AddDays(-7), Column3 = 0.75m, IsActive = 1 });
+                Insert.IntoTable("Sample").InSchema("dbo")
+                    .Row(new { Column1 = "Large Order Fee", Column2 = DateTime.Now.AddDays(-7), Column3 = 9.85m, IsActive = 1 });
+
+            }
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ DB_Migration/Migrations/Sprint05/AddColumnToUsers.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace DB_Migration.Migrations.Sprint05
+{
+    [Migration(201505242234)]
+    public class AddColumnToUsers : Migration
+    {
+        public override void Up()
+        {
+            if (!Schema.Table("Users").Column("CellPhone").Exists())
+            {
+                this.Create.Column("CellPhone").OnTable("Users").InSchema("dbo")
+                    .AsString(25).NotNullable().WithDefaultValue("Unknown");
+            }
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
