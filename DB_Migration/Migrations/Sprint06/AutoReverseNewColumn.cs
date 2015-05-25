57bb10c999e210340f74644e338c54361f6f6511
KCallis <keith.callis@sds-consulting.com>
2015-05-24 23:27:25 -0400

    [none] autoreverse migration

---
+++ DB_Migration/Migrations/Sprint06/AutoReverseNewColumn.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+
+namespace DB_Migration.Migrations.Sprint06
+{
+    [Migration(201505242309)]
+    public class AutoReverseNewColumn : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Rename.Column("Column1").OnTable("Sample").InSchema("dbo").To("FeeType");
+        }
+
+    }
+}
--- DB_Migration/dev-down.ps1
+++ DB_Migration/dev-down.ps1
@@ -1,1 +1,1 @@
-﻿.\MIGRATE -db SqlServer2008 -connection "data source=(local);initial catalog=FMSample;integrated security=True;" -a DB_Migration.dll -t migrate:down -version 201505232313 -tag Test
+﻿.\MIGRATE -db SqlServer2008 -connection "data source=(local);initial catalog=FMSample;integrated security=True;" -a DB_Migration.dll -t rollback:toversion -version 201505242234
